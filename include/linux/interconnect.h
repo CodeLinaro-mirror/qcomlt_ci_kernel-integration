434b7c4934d1db7a1b28bc9fc21c0e2128e89014
Georgi Djakov <georgi.djakov@linaro.org>
2018-06-25 16:57:39 +0300

    interconnect: Add support for path tags
    Consumers may have use cases with different bandwidth requirements based on
    the system or driver state. The consumer driver can append a specific tag to
    the path and pass this information to the interconnect platform driver to do
    the aggregation based on this state.
    
    Introduce set_path_tag() function that will allow the consumers to optionally
    append tag to each path. The aggregation of these tagged paths is platoform
    specific.
    
    Signed-off-by: Georgi Djakov <georgi.djakov@linaro.org>

--- include/linux/interconnect.h
+++ include/linux/interconnect.h
@@ -30,6 +30,7 @@ struct icc_path *icc_get(struct device *dev, const int src_id,
 struct icc_path *of_icc_get(struct device *dev, const char *name);
 void icc_put(struct icc_path *path);
 int icc_set(struct icc_path *path, u32 avg_bw, u32 peak_bw);
+void icc_set_tag(struct icc_path *path, u8 tag);
 
 #else
 
@@ -55,6 +56,10 @@ static inline int icc_set(struct icc_path *path, u32 avg_bw, u32 peak_bw)
 		return 0;
 
 	return -EOPNOTSUPP;
+}
+
+static inline void icc_set_tag(struct icc_path *path, u8 tag)
+{
 }
 
 #endif /* CONFIG_INTERCONNECT */
