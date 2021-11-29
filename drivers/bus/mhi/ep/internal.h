d5c6975a089ba52e4d2e4023444dd8f6a58320d4
Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
2021-11-29 12:47:50 +0530

    bus: mhi: ep: Add support for sending events to the host
    Add support for sending the events to the host over MHI bus from the
    endpoint. Following events are supported:
    
    1. Transfer completion event
    2. Command completion event
    3. State change event
    4. Execution Environment (EE) change event
    
    An event is sent whenever an operation has been completed in the MHI EP
    device. Event is sent using the MHI event ring and additionally the host
    is notified using an IRQ if required.
    
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>

--- drivers/bus/mhi/ep/internal.h
+++ drivers/bus/mhi/ep/internal.h
@@ -128,8 +128,8 @@ enum mhi_ep_ring_type {
 };
 
 struct mhi_ep_ring_element {
-	u64 ptr;
-	u32 dword[2];
+	__le64 ptr;
+	__le32 dword[2];
 };
 
 /* Ring element */
@@ -223,5 +223,9 @@ void mhi_ep_mmio_get_mhi_state(struct mhi_ep_cntrl *mhi_cntrl, enum mhi_state *s
 			       bool *mhi_reset);
 void mhi_ep_mmio_init(struct mhi_ep_cntrl *mhi_cntrl);
 void mhi_ep_mmio_update_ner(struct mhi_ep_cntrl *mhi_cntrl);
+
+/* MHI EP core functions */
+int mhi_ep_send_state_change_event(struct mhi_ep_cntrl *mhi_cntrl, enum mhi_state state);
+int mhi_ep_send_ee_event(struct mhi_ep_cntrl *mhi_cntrl, enum mhi_ep_execenv exec_env);
 
 #endif
