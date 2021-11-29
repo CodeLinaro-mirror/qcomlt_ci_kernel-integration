a2acdbc189ec12feb2149f3670d9eb0d83808e54
Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
2021-11-29 12:47:50 +0530

    bus: mhi: ep: Add support for sending events to the host
    Add support for sending the events to the host over MHI bus from the
    endpoint. Following events are supported:
    
    1. Transfer completion event
    2. Command completion event
    3. State change event
    4. Execution Environment (EE) change event
    
    An event is sent whenever an operation has been completed in the MHI EP
    device. Event is sent using the MHI event ring and additionally the host
    is notified using an IRQ if required.
    
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>

--- include/linux/mhi_ep.h
+++ include/linux/mhi_ep.h
@@ -67,6 +67,7 @@ struct mhi_ep_db_info {
  * @ch_db_list: List of queued channel doorbells
  * @st_transition_list: List of state transitions
  * @list_lock: Lock for protecting state transition and channel doorbell lists
+ * @event_lock: Lock for protecting event rings
  * @chdb: Array of channel doorbell interrupt info
  * @raise_irq: CB function for raising IRQ to the host
  * @alloc_addr: CB function for allocating memory in endpoint for storing host context
@@ -101,6 +102,7 @@ struct mhi_ep_cntrl {
 	struct list_head ch_db_list;
 	struct list_head st_transition_list;
 	spinlock_t list_lock;
+	struct mutex event_lock;
 	struct mhi_ep_db_info chdb[4];
 
 	void (*raise_irq)(struct mhi_ep_cntrl *mhi_cntrl);
