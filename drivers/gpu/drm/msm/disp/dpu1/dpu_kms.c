5ed1225eba9f800fba3871b9bc9b4933a5a955a1
Vinod Polimera <quic_vpolimer@quicinc.com>
2022-03-22 08:57:07 +0530

    drm/msm/disp/dpu1: set mdp clk to the maximum frequency in opp table during probe
    Set mdp clock to max clock rate during probe/bind sequence from the
    opp table so that rails are not at undetermined state. Since we do not
    know what will be the rate set in boot loader, it would be ideal to
    vote at max frequency. There could be a firmware display programmed
    in bootloader and we want to transition it to kernel without underflowing.
    The clock will be scaled down later when framework sends an update.
    
    Fixes: 25fdd5933e4c ("drm/msm: Add SDM845 DPU support")
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Tested-by: Douglas Anderson <dianders@chromium.org>
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>

--- drivers/gpu/drm/msm/disp/dpu1/dpu_kms.c
+++ drivers/gpu/drm/msm/disp/dpu1/dpu_kms.c
@@ -1209,7 +1209,9 @@ static int dpu_bind(struct device *dev, struct device *master, void *data)
 	struct platform_device *pdev = to_platform_device(dev);
 	struct drm_device *ddev = priv->dev;
 	struct dpu_kms *dpu_kms;
+	struct dev_pm_opp *opp;
 	int ret = 0;
+	unsigned long max_freq = ULONG_MAX;
 
 	dpu_kms = devm_kzalloc(&pdev->dev, sizeof(*dpu_kms), GFP_KERNEL);
 	if (!dpu_kms)
@@ -1231,6 +1233,12 @@ static int dpu_bind(struct device *dev, struct device *master, void *data)
 		return ret;
 	}
 	dpu_kms->num_clocks = ret;
+
+	opp = dev_pm_opp_find_freq_floor(dev, &max_freq);
+	if (!IS_ERR(opp))
+		dev_pm_opp_put(opp);
+
+	dev_pm_opp_set_rate(dev, max_freq);
 
 	platform_set_drvdata(pdev, dpu_kms);
 
