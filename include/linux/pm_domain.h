34fbf130bbebd81a69d0e4fce2c10b56d78e2326
Maulik Shah <quic_mkshah@quicinc.com>
2022-05-11 18:46:54 +0530

    PM: domains: Store the closest hrtimer event of the domain CPUs
    The arch timer can not wake up the Qualcomm Technologies, Inc. (QTI)
    SoCs when the deepest CPUidle modes results in the SoC also to enter
    the low power mode.
    
    RSC is part of CPU subsystem and APSS rsc device is attached to cluster
    power domain. RSC has to setup next hrtimer wakeup in CONTROL_TCS which
    can wakeup the SoC from deepest low power states. The CONTROL_TCS does
    this by writing next wakeup in always on domain timer when the SoC is
    entering the low power state.
    
    Add dev_pm_genpd_get_next_hrtimer() to get the genpd wakeup time.
    
    Signed-off-by: Maulik Shah <quic_mkshah@quicinc.com>

--- include/linux/pm_domain.h
+++ include/linux/pm_domain.h
@@ -17,6 +17,7 @@
 #include <linux/notifier.h>
 #include <linux/spinlock.h>
 #include <linux/cpumask.h>
+#include <linux/time64.h>
 
 /*
  * Flags to control the behaviour of a genpd.
@@ -136,6 +137,7 @@ struct generic_pm_domain {
 	struct gpd_dev_ops dev_ops;
 	s64 max_off_time_ns;	/* Maximum allowed "suspended" time. */
 	ktime_t next_wakeup;	/* Maintained by the domain governor */
+	ktime_t next_hrtimer;	/* Next hrtimer for the CPU PM domain */
 	bool max_off_time_changed;
 	bool cached_power_down_ok;
 	bool cached_power_down_state_idx;
@@ -228,6 +230,7 @@ int dev_pm_genpd_set_performance_state(struct device *dev, unsigned int state);
 int dev_pm_genpd_add_notifier(struct device *dev, struct notifier_block *nb);
 int dev_pm_genpd_remove_notifier(struct device *dev);
 void dev_pm_genpd_set_next_wakeup(struct device *dev, ktime_t next);
+ktime_t dev_pm_genpd_get_next_hrtimer(struct device *dev);
 
 extern struct dev_power_governor simple_qos_governor;
 extern struct dev_power_governor pm_domain_always_on_gov;
@@ -289,6 +292,10 @@ static inline int dev_pm_genpd_remove_notifier(struct device *dev)
 static inline void dev_pm_genpd_set_next_wakeup(struct device *dev, ktime_t next)
 { }
 
+static inline ktime_t dev_pm_genpd_get_next_hrtimer(struct device *dev)
+{
+	return KTIME_MAX;
+}
 #define simple_qos_governor		(*(struct dev_power_governor *)(NULL))
 #define pm_domain_always_on_gov		(*(struct dev_power_governor *)(NULL))
 #endif
